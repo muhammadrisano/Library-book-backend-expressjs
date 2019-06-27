17376d489cd6d40d89249a436a1f0bfa606d3d01
Muhammad Risano Akbar <muhammadrisano@gmail.com>
2019-06-27 11:35:54 +0700

    sudahlah

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 
 const mysql = require('mysql')
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'eno',
-    password: 'Qwer1234#',
-    database: 'library'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 })
 
 connection.connect((err) => {
