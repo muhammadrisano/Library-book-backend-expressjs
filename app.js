d50a09519617697828c3de7771962ed3945351f3
Muhammad Risano Akbar <muhammadrisano@gmail.com>
2019-06-28 09:44:31 +0700

    selesai

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.SERVER_PORT;
-// app.use(require('./src/routers/books'));
+
 const bookRoute = require('./src/routers/books')
 const categoryRoute = (require('./src/routers/category'))
 const information = require('./src/helpers/information')
