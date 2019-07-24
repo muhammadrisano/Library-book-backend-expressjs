720781fe3d5e02b096cf80664616cbab854756fd
Muhammad Risano Akbar <muhammadrisano@gmail.com>
2019-07-24 11:40:15 +0700

    get get

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,6 @@
 const userModels = require('../models/user');
 const MiscHelper = require('../helpers/helpers');
-
+const jwt = require('jsonwebtoken')
 module.exports = {
     getUser: (req, res) => {
         const search = req.query.search
@@ -20,6 +20,60 @@ module.exports = {
             .then((resultUser) => {
                 const result = resultUser
                 MiscHelper.response(res, result, 200)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    },
+    register: (req, res) => {
+
+        const salt = MiscHelper.generateSalt(18)
+        const passwordHash = MiscHelper.setPassword(req.body.password, salt)
+        const data = {
+            email: req.body.email,
+            card_number: req.body.card_number,
+            fullname: req.body.fullname,
+            password: passwordHash.passwordHash,
+            salt: passwordHash.salt,
+            token: '',
+            phone: req.body.phone,
+            job: req.body.job,
+            address: req.body.address,
+            status: 1,
+            role_id: 3,
+            created_at: new Date(),
+            updated_at: new Date()
+        }
+        console.log(data)
+        userModels.register(data)
+            .then((resultRegister) => {
+                MiscHelper.response(res, resultRegister, 200)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    },
+    login: (req, res) => {
+        const email = req.body.email
+        const password = req.body.password
+        console.log(req.headers['x-control-user']);
+        userModels.getByEmail(email)
+            .then((result) => {
+                const dataUser = result[0]
+
+                const usePassword = MiscHelper.setPassword(password, dataUser.salt).passwordHash
+
+                if (usePassword === dataUser.password) {
+                    dataUser.token = jwt.sign({
+                        userid: dataUser.id_user
+                    }, process.env.SECRET_KEY, { expiresIn: '1h' })
+
+                    delete dataUser.salt
+                    delete dataUser.password
+                    return MiscHelper.response(res, dataUser, 200)
+                } else {
+                    return MiscHelper.response(res, null, 403, 'Wrong password !')
+                }
             })
             .catch((error) => {
                 console.log(error)
--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,36 +1,60 @@
+crypto = require('crypto');
 module.exports = {
-    response: (res, result, status, data) => {
-        console.log(result);
+
+    response: (res, result, status, error) => {
         let resultPrint = {}
-        if (result.affectedRows > 0) {
-            if (result.insertId > 0) {
-                resultPrint.status = 'Success',
-                    resultPrint.status_code = 200,
-                    resultPrint.message = "Data success created !!",
-                    resultPrint.insertData = data
-            } else if (result.changedRows > 0) {
-                resultPrint.status = 'Success',
-                    resultPrint.status_code = 200,
-                    resultPrint.message = "Data success Updated !!",
-                    resultPrint.updateDate = data
-            } else {
-                resultPrint.Status = 'Success',
-                    resultPrint.status_code = 200,
-                    resultPrint.message = "Data success Deleted !!"
-            }
 
+        resultPrint.error = error || null
+        resultPrint.status_code = status || 200
+        resultPrint.result = result
 
-        } else if (result.length > 0) {
-            resultPrint.error = null
-            resultPrint.status_code = status || 200
-            resultPrint.jumlah = data
-            resultPrint.result = result
-
-        } else {
-            resultPrint.status = "Not Success"
-            resultPrint.status_code = 404
-            resultPrint.message = "Data Not Fount"
-        }
         return res.status(resultPrint.status_code).json(resultPrint)
+    },
+
+    // response: (res, result, status, data) => {
+    //     console.log(result);
+    //     let resultPrint = {}
+    //     if (result.affectedRows > 0) {
+    //         if (result.insertId > 0) {
+    //             resultPrint.status = 'Success',
+    //                 resultPrint.status_code = 200,
+    //                 resultPrint.message = "Data success created !!",
+    //                 resultPrint.insertData = data
+    //         } else if (result.changedRows > 0) {
+    //             resultPrint.status = 'Success',
+    //                 resultPrint.status_code = 200,
+    //                 resultPrint.message = "Data success Updated !!",
+    //                 resultPrint.updateDate = data
+    //         } else {
+    //             resultPrint.Status = 'Success',
+    //                 resultPrint.status_code = 200,
+    //                 resultPrint.message = "Data success Deleted !!"
+    //         }
+
+
+    //     } else if (result.length > 0) {
+    //         resultPrint.error = null
+    //         resultPrint.status_code = status || 200
+    //         resultPrint.jumlah = data
+    //         resultPrint.result = result
+
+    //     } else {
+    //         resultPrint.status = "Not Success"
+    //         resultPrint.status_code = 404
+    //         resultPrint.message = "Data Not Fount"
+    //     }
+    //     return res.status(resultPrint.status_code).json(resultPrint)
+    // },
+    generateSalt: (length) => {
+        return crypto.randomBytes(Math.ceil(length / 2)).toString('hex').slice(0, length)
+    },
+    setPassword: (password, salt) => {
+        let hash = crypto.createHmac('sha512', salt)
+        hash.update(password)
+        let value = hash.digest('hex')
+        return {
+            salt: salt,
+            passwordHash: value
+        }
     }
 }
