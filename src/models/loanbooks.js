37da66e754c322e9c60f8f513deb5f8a38125481
Muhammad Risano Akbar <muhammadrisano@gmail.com>
2019-08-07 07:10:34 +0700

    fix backend

--- src/models/loanbooks.js
+++ src/models/loanbooks.js
@@ -36,6 +36,28 @@ module.exports = {
             })
         })
     },
+    loanbooksConfirm: () => {
+        return new Promise((resolve, reject) => {
+            connection.query('SELECT `loan_book`.*, `user`.`fullname`, `user`.`phone`, `book_manager`.`name` AS `title`, `book_manager`.`image`,`book_manager`.`writer` FROM loan_book INNER JOIN user ON loan_book.card_number = user.card_number INNER JOIN book_manager ON loan_book.id_book = book_manager.id_book WHERE `loan_book`.`information` = ? ORDER BY id_loanbook DESC', 'PENDING', (err, result) => {
+                if (!err) {
+                    resolve(result)
+                } else {
+                    reject(new Error(err))
+                }
+            })
+        })
+    },
+    prosesConfirm: (id_loanbook, data) => {
+        return new Promise((resolve, reject) => {
+            connection.query('UPDATE loan_book SET ? WHERE id_loanbook=?', [data, id_loanbook], (err, result) => {
+                if (!err) {
+                    resolve(result)
+                } else {
+                    reject(new Error(err))
+                }
+            })
+        })
+    },
     loanbooksDetail: (id_loanbook) => {
         return new Promise((resolve, reject) => {
             console.log(id_loanbook)
@@ -59,10 +81,24 @@ module.exports = {
             })
         })
     },
+    updateborrow: (id_loanbook, data, id_book) => {
+        return new Promise((resolve, reject) => {
+            connection.query('UPDATE loan_book SET ? WHERE id_loanbook=?', [data, id_loanbook], (err, result) => {
+                if (!err) {
+                    connection.query('UPDATE book_manager SET status = 1 WHERE id_book =?', id_book)
+                    resolve(result)
+                } else {
+                    reject(new Error(err))
+                }
+            })
+        })
+    },
     insertLoanbooks: (data) => {
         return new Promise((resolve, reject) => {
+
             connection.query('INSERT INTO loan_book SET ?', data, (err, result) => {
                 if (!err) {
+                    connection.query('UPDATE book_manager SET status= 0 WHERE id_book =?', data.id_book)
                     resolve(result)
                 } else {
                     reject(new Error(err))
@@ -74,6 +110,18 @@ module.exports = {
         return new Promise((resolve, reject) => {
             connection.query('DELETE FROM loan_book WHERE id_loanbook = ?', id_loanbooks, (err, result) => {
                 if (!err) {
+                    resolve(result)
+                } else {
+                    reject(new Error(err))
+                }
+            })
+        })
+    },
+    cancelBorrow: (id_loanbooks, id_book) => {
+        return new Promise((resolve, reject) => {
+            connection.query('DELETE FROM loan_book WHERE id_loanbook = ?', id_loanbooks, (err, result) => {
+                if (!err) {
+                    connection.query('UPDATE book_manager SET status= 1 WHERE id_book =?', id_book)
                     resolve(result)
                 } else {
                     reject(new Error(err))
--- src/routers/loanbooks.js
+++ src/routers/loanbooks.js
@@ -4,11 +4,16 @@ const LoanbooksController = require('../controllers/loanbooks');
 const Auth = require('../helpers/auth')
 Route
     .all('/*', Auth.authInfo)
+    .patch('/updateborrow/:id_loanbook', LoanbooksController.updateborrow)
     .get('/', LoanbooksController.getLoanbooks)
     .get('/:id_loanbook', LoanbooksController.loanbooksDetail)
     .get('/cardnumber/:card_number', LoanbooksController.loanbooksCardnumber)
+    .get('/confirm/get', LoanbooksController.loanbooksConfirm)
+    .patch('/prosesconfirm/:id_loanbook', LoanbooksController.prosesConfirm)
     .patch('/:id_loanbook', LoanbooksController.updateLoanbooks)
     .post('/', Auth.accesstoken, LoanbooksController.insertLoanbooks)
     .delete('/:id_loanbook', LoanbooksController.deleteLoanbooks)
+    .patch('/cancelborrow/:id_loanbook', LoanbooksController.cancelBorrow)
+
 
 module.exports = Route;
